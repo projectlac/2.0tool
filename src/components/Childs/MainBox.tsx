d7a623aee7eee0caad9dbef42e5c7932f9f9b8d2
projectlac <dft1711198@gmail.com>
2022-03-25 23:20:43 +0700

    update Frame

--- src/components/Childs/MainBox.tsx
+++ src/components/Childs/MainBox.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Item } from 'src/models/item';
 import { SSR } from '../../content/data';
-
+import { ReactComponent as Search } from '../../assets/search.svg';
 interface propsFromHome {
   actionBan: (id: number) => void;
   mode: string;
@@ -33,23 +33,29 @@ function MainBox({ actionBan, mode }: propsFromHome) {
   return (
     <>
       <div className="MainPicker">
-        <input type="text" onChange={handleChangeInput} />
         <div className="MainPicker-box">
-          {convertData().map((shiki, index) => (
-            <div
-              className={`col-md-1-custom ${
-                itemHasPending === shiki.id ? 'choose' : ''
-              }`}
-              key={index}
-              onClick={() => {
-                pending(shiki.id);
-              }}
-            >
-              <img src={`/images/${shiki.img}`} alt={shiki.name} />
-            </div>
-          ))}
+          <div className="MainPicker-action">
+            <input type="text" onChange={handleChangeInput} />
+            <button onClick={handleAction}>
+              {mode === 'BAN' ? <Search /> : ' PICK'}
+            </button>
+          </div>
+          <div className="MainPicker-box-item">
+            {convertData().map((shiki, index) => (
+              <div
+                className={`col-md-1-custom ${
+                  itemHasPending === shiki.id ? 'choose' : ''
+                }`}
+                key={index}
+                onClick={() => {
+                  pending(shiki.id);
+                }}
+              >
+                <img src={`/images/${shiki.img}`} alt={shiki.name} />
+              </div>
+            ))}
+          </div>
         </div>
-        <button onClick={handleAction}>{mode}</button>
       </div>
       <div className="Onmyoji-box">
         <img src="/images/Seimei.png" alt="" />
